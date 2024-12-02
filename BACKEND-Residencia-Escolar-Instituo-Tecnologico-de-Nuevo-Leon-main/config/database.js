8f62e3fa6c95dcafdd85656a9789d4b4e3900059
samcrdz <samys1909@hotmail.com>
2024-12-01 18:28:45 -0600

    Update database.js

--- BACKEND-Residencia-Escolar-Instituo-Tecnologico-de-Nuevo-Leon-main/config/database.js
+++ BACKEND-Residencia-Escolar-Instituo-Tecnologico-de-Nuevo-Leon-main/config/database.js
@@ -1,11 +1,19 @@
+/* 
+  pg es una libreria que permite conectarnos a la base de datos creando una instancia con el objeto Pool
+
+  despues le pasamos los parametros a una constante que es la que nos permite acceder a la base de datos alojada en nuestro servidor local
+
+  el metodo connect nos permite testear la conexion a la base de datos, en este caso ponemos dos logs como debug para verificar la salida del connect
+*/
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',
   password: '12345',
   host: 'localhost',
-  port: 5432,
-  database: 'sistema_inventario'
+  port: 5433,
+  database: 'sistema _inventario'
 })
 
 pool.connect((error) => {
